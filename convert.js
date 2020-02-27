824efc42383618c7efc2ea00e26ec7f15e4ecb23
sergey <serge.dmitriev@gmail.com>
2020-02-27 20:47:04 +0700

    fix

---
+++ convert.js
@@ -0,0 +1,50 @@
+#!/usr/bin/node
+
+const image2base64 = require('image-to-base64');
+const fs = require('fs');
+const images = [
+  {
+    name: 'smartphone',
+    file: 'images/black/smartphone.png',
+  },
+  {
+    name: 'server',
+    file: 'images/black/server.png',
+  },
+  {
+    name: 'database',
+    file: 'images/black/database.png',
+  },
+  {
+    name: 'client',
+    file: 'images/black/client.png',
+  },
+];
+
+const cv = async function(image) {
+  return {
+    name: image.name,
+    data: await image2base64(image.file),
+  };
+}
+
+const run2 = async function () {
+  let imagesData = [];
+  for (const image of images){
+    imagesData.push(await cv(image));
+  }
+
+  var stream = fs.createWriteStream("icons.js", {flags:'w'});
+  stream.write('const icons = {\n');
+  imagesData.map(image => {
+    stream.write('  ' + image.name + ': ' + '"data:image/png;base64,' + image.data + '",\n');
+  })
+  stream.write('};\n');
+  stream.end;
+};
+
+try {
+  run2();
+} catch (e) {
+  console.log(e);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,33 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const concat = require('gulp-concat');
+const exec = require('child_process').exec;
 
-const arrCode = [    
-    'node_modules/cytoscape/dist/cytoscape.min.js',
-    'node_modules/numeric/numeric-1.2.6.min.js',
-    'node_modules/layout-base/layout-base.js',
-    'node_modules/cose-base/cose-base.js',
-    'node_modules/cytoscape-fcose/cytoscape-fcose.js',
-    'code.js',
+const filesConcatArray = [
+  'node_modules/cytoscape/dist/cytoscape.min.js',
+  'node_modules/numeric/numeric-1.2.6.min.js',
+  'node_modules/layout-base/layout-base.js',
+  'node_modules/cose-base/cose-base.js',
+  'node_modules/cytoscape-fcose/cytoscape-fcose.js',
+  'icons.js',
+  'code.js',
 ];
 
-gulp.task('client', (done) => {
-  gulp.src(arrCode)
+gulp.task('icons', function (cb) {
+  exec('node ./convert.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+gulp.task('concat', (done) => {
+  gulp.src(filesConcatArray)
     .pipe(concat('code-full.min.js'))
     .pipe(gulp.dest('dist'))
     .on('error', gutil.log);
   done();
 });
 
-gulp.task('default', gulp.series('client'));
+gulp.task('default', gulp.series('icons', 'concat'));
 
